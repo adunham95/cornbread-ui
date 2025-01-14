a844166265158b33e9b38be7aedac2df454fc096
Adrian <adunham95@gmail.com>
2025-01-14 10:27:27 -0500

    Working on docs site

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,29 @@
-import { mdsvex } from "mdsvex";
-import adapter from "@sveltejs/adapter-static";
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const pkg = JSON.parse(json);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://svelte.dev/docs/kit/integrations
-    // for more information about preprocessors
-    preprocess: [vitePreprocess(), mdsvex()],
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: [vitePreprocess(), mdsvex()],
 
-    kit: {
+	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
 	},
-
-    extensions: [".svelte", ".svx"]
+	version: {
+		name: pkg.version
+	},
+	extensions: ['.svelte', '.svx']
 };
 
 export default config;
