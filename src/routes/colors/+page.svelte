69ac7fb90a96486827c1bece2ffe44c8d8f1ba82
Adrian <adunham95@gmail.com>
2025-01-28 20:36:51 -0500

    Added complete list of colors

--- src/routes/colors/+page.svelte
+++ src/routes/colors/+page.svelte
@@ -31,4 +31,28 @@
 	<ColorSwatch color="bg-content-3" title="Content 3" />
 	<ColorSwatch color="bg-content-4" title="Content 4" />
 	<ColorSwatch color="bg-content-5" title="Content 5" />
+
+	<ColorSwatch color="bg-brand" title="Brand" />
+	<ColorSwatch color="bg-brand-1" title="Brand 1" />
+	<ColorSwatch color="bg-brand-2" title="Brand 2" />
+	<ColorSwatch color="bg-brand-3" title="Brand 3" />
+	<ColorSwatch color="bg-brand-4" title="Brand 4" />
+	<ColorSwatch color="bg-brand-5" title="Brand 5" />
+	<ColorSwatch color="bg-brand-content" title="Brand Content" />
+
+	<ColorSwatch color="bg-accent" title="Accent" />
+	<ColorSwatch color="bg-accent-1" title="Accent 1" />
+	<ColorSwatch color="bg-accent-2" title="Accent 2" />
+	<ColorSwatch color="bg-accent-3" title="Accent 3" />
+	<ColorSwatch color="bg-accent-4" title="Accent 4" />
+	<ColorSwatch color="bg-accent-5" title="Accent 5" />
+	<ColorSwatch color="bg-accent-content" title="Accent Content" />
+
+	<ColorSwatch color="bg-disabled" title="disabled" />
+	<ColorSwatch color="bg-disabled-1" title="disabled 1" />
+	<ColorSwatch color="bg-disabled-2" title="disabled 2" />
+	<ColorSwatch color="bg-disabled-3" title="disabled 3" />
+	<ColorSwatch color="bg-disabled-4" title="disabled 4" />
+	<ColorSwatch color="bg-disabled-5" title="disabled 5" />
+	<ColorSwatch color="bg-disabled-content" title="disabled-content" />
 </div>
